7bff54e7950d897ce236d5644c2aeff528c8fd2f
Fritz Dittmer <fdittmer9762@gmail.com>
2016-11-06 13:19:00 -0700

    Coroutines/Delegates scripts
    created two scripts that move a cube, one using update, one using a coroutine to demonstrate the difference and benifits fo a coroutine,
    will noteate later

---
+++ DGM2670/Assets/Scripts/isAcoroutine.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isAcorutine : MonoBehaviour
+{
+
+    delegate void DelegateType();
+
+    DelegateType delA, delB, delC;
+
+    void Start()
+    {
+        delA = Thing;
+        delB = OtherThing;
+        delC = OtherOtherThing;
+
+        DelegateType delD = () =>
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        };
+
+        StartCoroutine(Move(Vector3.forward, delD));
+
+        delD = Thing;
+    }
+
+    void Thing()
+    {
+        transform.localScale *= 2;
+    }
+
+    void OtherThing()
+    {
+        transform.localScale /= 2;
+    }
+
+    void OtherOtherThing()
+    {
+        transform.Rotate(new Vector3(0, 45, 0));
+    }
+
+    public float timeStep;
+
+    IEnumerator Move(Vector3 dir, DelegateType thing)
+    {
+        float t = 0;
+        float duration = timeStep;
+        GetComponent<Renderer>().material.color = Color.blue;
+        while (t < timeStep)
+        {
+            transform.Translate(dir * Time.deltaTime);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        t = 0;
+        GetComponent<Renderer>().material.color = Color.red;
+        thing();
+        yield return new WaitForSeconds(timeStep);
+        Vector3 newDir = Quaternion.Euler(0, 90, 0) * dir;
+
+        StartCoroutine(Move(newDir, thing));
+    }
+}
---
+++ DGM2670/Assets/Scripts/notACoroutine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotCoroutine : MonoBehaviour {
+
+    public float timeStep = 1;
+    float t = 0;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (t < timeStep)
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+            transform.Translate(Vector3.forward * Time.deltaTime);
+
+        }
+
+        else if (t < timeStep * 2)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+
+        if (t > timeStep * 2)
+            t = 0;
+
+        t += Time.deltaTime;
+    }
+}
