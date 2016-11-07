f1dc860ce380c405f4f6c1c5962578bdf43ada34
Fritz Dittmer <fdittmer9762@gmail.com>
2016-11-06 18:27:49 -0700

    Events Study/Notes
    looking into and creating some events
    emulated event system (to disect)

---
+++ DGM2670/Assets/Scripts/PriceChangedArgs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PriceChangedArgs : EventArgs {
+
+    public readonly decimal LastPrice, NewPrice;
+
+    public PriceChangedArgs(decimal lastPrice, decimal newPrice) {
+        LastPrice = lastPrice; NewPrice = newPrice;
+    }
+
+}
---
+++ DGM2670/Assets/Scripts/Stock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stock {
+
+    string symbol; decimal price;
+
+    public Stock(string symbol) { this.symbol = symbol; } //not sure what there doing here, they are creating a symbol that is within and then setting it to itself; or so it seems
+
+    public event EventHandler<PriceChangedArgs> Pricechanged;
+
+    protected virtual void OnPriceChanged(PriceChangedArgs e) {
+        if (Pricechanged != null)
+            Pricechanged(this, e);
+    }
+
+    public decimal Price {
+        get { return price; }
+        set {
+            if (price == value) return;
+
+            OnPriceChanged(new PriceChangedArgs(price, value));
+            /*if (Pricechanged != null){
+                Pricechanged(price, value);
+            }*/
+
+            price = value;
+        }
+    }
+}
