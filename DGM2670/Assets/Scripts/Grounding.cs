60d125ec94a1b679c5bba5964c2cf4b461b9ce72
Fritz Dittmer <fdittmer9762@gmail.com>
2016-11-12 15:04:34 -0700

    PlayerController Example
    player controller class example

---
+++ DGM2670/Assets/Scripts/Grounding.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class Grounding : MonoBehaviour {
+
+    private float gravity, jumpLimit, jumpForce;
+    private CharacterController cc;
+    private Vector3 tempP;
+
+
+    void Start() {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        tempP.y = gravity;
+        tempP.y += Jump(Input.GetAxis("Vertical"));
+        cc.Move(tempP * Time.deltaTime);
+    }
+
+    float Move(float mov) {
+        return mov;
+    }
+
+    float Jump(float jmp) {
+        if (jumpLimit > 0){
+             jumpLimit-= Time.deltaTime;
+            return jmp * jumpForce;
+        }
+        if (cc.isGrounded) {
+            jumpLimit = 2;
+            return 0;
+        }
+    }
+
+}
